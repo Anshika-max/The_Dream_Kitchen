4418d98092d2d9f06b8e4ad5de2aac5f2e1a7523
Anshika Srivastava <anshika82d@gmail.com>
2025-08-30 16:36:02 +0530

    Deployment final

--- frontend/src/LandingPage/admin/ManageOrder.jsx
+++ frontend/src/LandingPage/admin/ManageOrder.jsx
@@ -1,4 +1,20 @@
 import React, { useEffect } from "react";
+import {
+  Container,
+  Typography,
+} from "@mui/material";
+
 export default function ManageOrder(){
-    return(<>Order Page</>);
+    return(
+        <Container maxWidth="md" sx={{ textAlign: "center", mt: 10 }}>
+        <Typography variant="h5" color="text.secondary">
+            Sorry!!
+            <br></br>
+          View Order is empty due to Payment Gateway is yet not supported by our website. .
+          <br></br>
+          <br></br>
+          <br></br>
+        </Typography>
+      </Container>
+    );
 }
